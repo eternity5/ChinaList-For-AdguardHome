8bca991154801ec34dff1e131ac6152aeabe1fc6
eternity5 <74863705+eternity5@users.noreply.github.com>
2023-06-08 23:15:07 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,5 @@ wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/acc
 sed -i -e 's/server=/[/g' -e 's/114.114.114.114/]https:\/\/dns.alidns.com\/dns-query/g' ChinaList.conf
 echo "tls://8.8.4.4" >> ChinaList.conf
 echo "tls://dns.google" >> ChinaList.conf
+echo >> ChinaList.conf
 mv ChinaList.conf ChinaList.txt
